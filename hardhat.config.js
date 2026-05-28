07840bce3ee8b9b07831b5fa47ac43fb360f6f84
pausalipaloi <91822207+pousalipaloi@users.noreply.github.com>
2026-05-28 20:43:06 +0530

    Add Hardhat configuration for Solidity and networks

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      viaIR: true,
+      optimizer: { enabled: true, runs: 200 }
+    }
+  },
+  networks: {
+    hardhat: {},
+    localhost: { url: "http://127.0.0.1:8545" }
+  }
+};
